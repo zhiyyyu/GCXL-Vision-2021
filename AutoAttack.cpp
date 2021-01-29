c130242612bf1da1a1f065cefdaa77ad350189ae
nArrow4 <3284799532@qq.com>
2021-01-29 22:09:04 +0800

    merge some methods

--- AutoAttack.cpp
+++ AutoAttack.cpp
@@ -2,12 +2,22 @@
 
 namespace QRCode{
     RoboMasterS1::RoboMasterS1() {
-
+//        thread1_ = new thread1();
+//        thread2_ = new thread2();
+        solvePNP_ = new solvePNP();
+        transform_ = new transform();
+        detect_ = new detect();
+    }
+    RoboMasterS1::~RoboMasterS1(){
+//        delete thread1_;
+//        delete thread2_;
+        delete solvePNP_;
+        delete transform_;
     }
     void RoboMasterS1::attack(){
-        solvePNP* solvePNP_ = new solvePNP();
-        solvePNP_->detect();
-        solvePNP_->display();
-        solvePNP_->solve();
+//        solvePNP_->detect();
+//        solvePNP_->display();
+//        solvePNP_->solve();
+//        transform_->setArmor2World(solvePNP_->TransformMatrix);
     }
 }
--- AutoAttack.h
+++ AutoAttack.h
@@ -2,6 +2,10 @@
 #define QRCODE_AUTOATTACK_H
 
 #include "solvePnP.h"
+#include "transform.h"
+#include "thread1.h"
+#include "thread2.h"
+#include "detect.h"
 
 namespace QRCode{
     class RoboMasterS1{
@@ -9,6 +13,13 @@ namespace QRCode{
         RoboMasterS1();
         ~RoboMasterS1();
         void attack();
+
+        thread1* thread1_;
+        thread2* thread2_;
+        solvePNP* solvePNP_;
+        transform* transform_;
+        detect* detect_;
+//        Sophus::SE3 armor2camera;
     };
 }
 
