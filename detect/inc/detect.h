e1c0137c81eee964fbbc39349f252f5f7f1c1407
nArrow4 <3284799532@qq.com>
2021-02-14 23:01:34 +0800

    update detector

--- detect/inc/detect.h
+++ detect/inc/detect.h
@@ -2,9 +2,17 @@
 #define QRCODE_DETECT_H
 
 #include "score.h"
+#include "numeric"
+#include <cmath>
 
-#define DEBUG 1
+#define DEBUG 0
 #define LABEL_DEBUG 1
+#define CONTOURS_DEBUG 0
+#define IMAGE_SHOW 0
+#define FEATURE_DEBUG 0
+#define SHOW_ROI 1
+
+#define PI (3.14)
 
 namespace QRCode{
     class detect{
@@ -16,10 +24,69 @@ namespace QRCode{
         cv::Mat getROI(cv::Mat img, cv::Point2f tl, cv::Point2f tr, cv::Point2f bl);
         float calInnerProduct(cv::Point2f p1, cv::Point2f p2, cv::Point2f p3);
         float calOuterProduct(cv::Point2f p1, cv::Point2f p2, cv::Point2f p3);
+        bool judgeFlags(cv::Mat img, std::vector<cv::Point> contour);
 
         int thresh = 150;
         int maxval = 255;
     };
+
+    typedef struct labelFlag{
+        float w;
+        float h;
+        float widthHeightRatio;
+        /**
+         *     _____
+         *     \    \
+         *      \    \
+         *       \    \
+         *        \____\_angle__
+         *    |
+         *    |
+         *    |____ 90 degree
+         *
+         *     ____ 0 degree
+         *
+         */
+        float angle;
+        cv::Point2f center;
+        cv::Point2f tl;
+        cv::Point2f points[5];
+        std::vector<int> judgePoints = {0, 0, 0, 0, 0};
+        float offset = 5;
+        float getWidthHeightRatio(){
+            return w / h;
+        }
+        void setPointsFromCenter(cv::Point2f* points){
+            /**points:
+             *          0
+             *      1   2   3
+             *          4
+             */
+            float cosAngle = cos(angle/180*PI);
+            float sinAngle = sin(angle/180*PI);
+            float ht = h-offset*2;
+            float wd = w-offset*2;
+            points[0] = {center.x-ht/2*sinAngle, center.y-ht/2*cosAngle};
+            points[1] = {center.x-wd/2*cosAngle, center.y+wd/2*sinAngle};
+            points[2] = center;
+            points[3] = {center.x+wd/2*cosAngle, center.y-wd/2*sinAngle};
+            points[4] = {center.x+ht/2*sinAngle, center.y+ht/2*cosAngle};
+            return;
+        }
+        void setPointsFromTopLeft(cv::Point2f* points){
+            /**points:
+             *          0
+             *      1   2   3
+             *          4
+             */
+            points[0] = {tl.x+w/2, tl.y+offset};
+            points[1] = {tl.x+offset, tl.y+h/2};
+            points[2] = {tl.x+w/2, tl.y+h/2};
+            points[3] = {tl.x+w-offset, tl.y+h/2};
+            points[4] = {tl.x+w/2, tl.y+h-offset};
+            return;
+        }
+    } labelFlag;
 }
 
 #endif //QRCODE_DETECT_H
--- detect/src/detect.cpp
+++ detect/src/detect.cpp
@@ -15,15 +15,22 @@ namespace QRCode{
 //        score_->display<cv::Ptr<cv::ml::Boost>>(score_->boost);
     }
     std::vector<cv::RotatedRect> detect::getDirectionFlags(cv::Mat image){
-        cv::Mat grayImg, threshImg, harris, showImg = image.clone();
+        cv::Mat grayImg, threshImg, harris, blurImg, showImg = image.clone();
         std::vector<std::vector<cv::Point>> contours;
         std::vector<cv::RotatedRect> minRect;
         cv::cvtColor(image, grayImg, cv::COLOR_BGR2GRAY);
         cv::threshold(grayImg, threshImg, thresh, maxval, cv::THRESH_BINARY);
-        cv::findContours(threshImg, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+        blurImg = threshImg.clone();
+//        cv::GaussianBlur(threshImg, blurImg, cv::Size(5, 5), 3, 3);
+//        cv::bilateralFilter(threshImg, blurImg, 10, 0, 0);
+        cv::findContours(blurImg, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+//        std::vector<std::vector<cv::Point>> polyContours(contours.size());
         for(int i=0; i<contours.size(); i++){
+//            cv::approxPolyDP(contours[i], polyContours[i], 20, true);
             //@TODO 更换检测策略
-            if(contours[i].size() == 11){
+//            if(contours[i].size() == 11)
+            if(judgeFlags(blurImg, contours[i]))
+            {
                 minRect.emplace_back(cv::minAreaRect(contours[i]));
                 cv::drawContours(showImg, contours, i, cv::Scalar(0, 255, 0), 3);
 #if DEBUG
@@ -31,21 +38,59 @@ namespace QRCode{
 #endif
             }
         }
-#if DEBUG
+#if CONTOURS_DEBUG
         for(int i=0; i<contours.size(); i++) {
             std::cout << contours[i].size() << std::endl;
+            std::cout << judgeFlags(blurImg, contours[i]);
             for (int j = 0; j < contours[i].size(); j++) {
                 std::cout << contours[i].at(j) << std::endl;
             }
         }
         cv::drawContours(showImg, contours, -1, cv::Scalar(0, 255, 0), 3);
         std::cout << contours.size() << std::endl;
+#endif
+#if IMAGE_SHOW
         cv::imshow("gray", grayImg);
+        cv::imshow("blur", blurImg);
         cv::imshow("thresh", threshImg);
         cv::imshow("contours", showImg);
         cv::waitKey(0);
 #endif
         return minRect;
+    }
+
+    bool detect::judgeFlags(cv::Mat img, std::vector<cv::Point> contour) {
+        labelFlag label;
+        cv::RotatedRect minRect = cv::minAreaRect(contour);
+//        cv::Rect rect = minRect.boundingRect();
+//        label.w = rect.width;
+//        label.h = rect.height;
+//        label.widthHeightRatio = label.getWidthHeightRatio();
+//        label.tl = rect.tl();
+//        label.setPointsFromTopLeft(label.points);
+        label.w = minRect.size.width;
+        label.h = minRect.size.height;
+        label.angle = -minRect.angle;
+        label.widthHeightRatio = label.getWidthHeightRatio();
+        label.center = minRect.center;
+        label.setPointsFromCenter(label.points);
+        for(int i=0; i<5; i++) {
+            cv::Point2f judgePoint = label.points[i];
+//            std::cout << img.size << std::endl;
+            label.judgePoints[i] = img.at<uchar>((int) judgePoint.y, (int) judgePoint.x) == 0;
+        }
+#if FEATURE_DEBUG
+        std::cout << label.widthHeightRatio << label.points[0] << label.angle << std::endl;
+        std::cout << label.judgePoints[0] << label.judgePoints[1] << label.judgePoints[2] << label.judgePoints[3] << label.judgePoints[4] << std::endl;
+        std::cout << "sum " << std::accumulate(label.judgePoints.begin(), label.judgePoints.end(), 0) << std::endl;
+        for(int i=0; i<5; i++){
+            cv::circle(img, label.points[i], 1, cv::Scalar(255, 0, 0));
+        }
+        cv::imshow("points", img);
+        cv::waitKey(0);
+#endif
+        return std::accumulate(label.judgePoints.begin(), label.judgePoints.end(), 0) == 2 &&
+                label.widthHeightRatio >= 0.5 && label.widthHeightRatio <= 1.5;
     }
 
     cv::Mat detect::correctVisualLabel(cv::Mat img, std::vector<cv::RotatedRect> rect) {
@@ -66,7 +111,8 @@ namespace QRCode{
             float innerProduct, outerProduct;
             cv::Point2f tl, tr, bl;
             innerProduct = calInnerProduct(centerPoint[0], centerPoint[1], centerPoint[2]);
-            if(innerProduct == 0){
+//            std::cout << "inner " << innerProduct << std::endl;
+            if(innerProduct <= 100 && innerProduct >= -100){
                 outerProduct = calOuterProduct(centerPoint[0], centerPoint[1], centerPoint[2]);
                 tl = centerPoint[0];
                 tr = outerProduct>0?centerPoint[1]:centerPoint[2];
@@ -74,7 +120,8 @@ namespace QRCode{
                 return getROI(img, tl, tr, bl);
             }
             innerProduct = calInnerProduct(centerPoint[1], centerPoint[0], centerPoint[2]);
-            if(innerProduct == 0){
+//            std::cout << "inner " << innerProduct << std::endl;
+            if(innerProduct <= 100 && innerProduct >= -100){
                 outerProduct = calOuterProduct(centerPoint[1], centerPoint[0], centerPoint[2]);
                 tl = centerPoint[1];
                 tr = outerProduct>0?centerPoint[0]:centerPoint[2];
@@ -82,7 +129,8 @@ namespace QRCode{
                 return getROI(img, tl, tr, bl);
             }
             innerProduct = calInnerProduct(centerPoint[2], centerPoint[0], centerPoint[1]);
-            if(innerProduct == 0){
+//            std::cout << "inner " << innerProduct << std::endl;
+            if(innerProduct <= 100 && innerProduct >= -100){
                 outerProduct = calOuterProduct(centerPoint[2], centerPoint[0], centerPoint[1]);
                 tl = centerPoint[2];
                 tr = outerProduct>0?centerPoint[0]:centerPoint[1];
@@ -102,17 +150,23 @@ namespace QRCode{
     }
 
     cv::Mat detect::getROI(cv::Mat img, cv::Point2f tl, cv::Point2f tr, cv::Point2f bl) {
-//        int width = (int)(tr.x-tl.x);
-//        int height = (int)(bl.y-tl.y);
-//        cv::Mat ROI = img(cv::Rect(tl.x, tl.y, width, height));
+        int width = (int)(tr.x-tl.x);
+        int height = (int)(bl.y-tl.y);
         cv::Mat warpMat = cv::Mat(2, 3, CV_32FC1);
         cv::Mat warpImg = img.clone();
         cv::Point2f srcPoints[3] = {tl, tr, bl};
-        cv::Point2f dstPoints[3] = {tl, {tl.x+128, tl.y}, {tl.x, tl.y+128}};
+        cv::Point2f dstPoints[3] = {tl, {tl.x+width, tl.y}, {tl.x, tl.y+height}};
         warpMat = cv::getAffineTransform(srcPoints, dstPoints);
         cv::warpAffine(img, warpImg, warpMat, warpImg.size());
+        cv::Mat ROI = warpImg(cv::Rect((int)tl.x, (int)tl.y, width, height));
+#if SHOW_ROI
+        for(int i=0; i<3; i++) {
+            cv::circle(warpImg, dstPoints[i], 1, cv::Scalar(255, 0, 0));
+        }
         cv::imshow("warpImg", warpImg);
+        cv::imshow("ROI", ROI);
         cv::waitKey(0);
-        return warpImg;
+#endif
+        return ROI;
     }
 }
