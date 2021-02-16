b4013ceac57e5a9b6930a86182e09cf6f8a902b2
nArrow4 <3284799532@qq.com>
2021-02-16 17:46:24 +0800

    auto attack

--- AutoAttack.cpp
+++ AutoAttack.cpp
@@ -1,7 +1,7 @@
 #include "AutoAttack.h"
 
 namespace QRCode{
-    RoboMasterS1::RoboMasterS1() {
+    Robot::Robot() {
 //        thread1_ = new thread1();
 //        thread2_ = new thread2();
 //        solvePNP_ = new solvePNP();
@@ -9,7 +9,7 @@ namespace QRCode{
         detect_ = new detect();
         locate_ = new locate();
     }
-    RoboMasterS1::~RoboMasterS1(){
+    Robot::~Robot(){
 //        delete thread1_;
 //        delete thread2_;
         delete solvePNP_;
@@ -17,16 +17,36 @@ namespace QRCode{
         delete detect_;
         delete locate_;
     }
-    void RoboMasterS1::attack(){
-        cv::Mat img = cv::imread(locate_->root + "/3.png");
+    void Robot::train(){
+        cv::Mat trainMat, labelMat;
+        std::pair<cv::Mat, cv::Mat> data = locate_->loadData();
+        trainMat = data.first, labelMat = data.second;
+        locate_->decode(trainMat, labelMat);
+        std::cout << labelMat.size << labelMat << std::endl;
+    }
+
+    void Robot::attack(){
+        cv::Mat img = cv::imread(locate_->root + "/5.png");
+        cv::imshow("img", img);
         std::vector<cv::RotatedRect> rect;
         rect = detect_->getDirectionFlags(img);
-        cv::Mat visualLabel;
-        visualLabel = detect_->correctVisualLabel(img.clone(), rect);
-//        cv::Mat trainMat, labelMat;
-//        std::pair<cv::Mat, cv::Mat> data = locate_->loadData();
-//        trainMat = data.first, labelMat = data.second;
-//        locate_->decode(trainMat, labelMat);
+        cv::Mat visualLabel = detect_->correctVisualLabel(img.clone(), rect);
+        std::pair<cv::Mat, cv::Mat> twoParts = locate_->divideIntoTwoParts(visualLabel);
+        cv::Mat leftHalf = twoParts.first;
+        cv::Mat rightHalf = twoParts.second;
+//        cv::imshow("left", leftHalf);
+//        cv::imshow("right", rightHalf);
+//        cv::waitKey(0);
+        int leftOcr = locate_->predict(leftHalf);
+        int rightOcr = locate_->predict(rightHalf);
+        std::cout << leftOcr << rightOcr << std::endl;
+        std::vector<int> location = locate_->getLocation(leftOcr*10+rightOcr);
+//        for(int i=0; i<4; i++){
+//            std::cout << location[i] << std::endl;
+//        }
+        cv::putText(visualLabel, locate_->getString(location), cv::Point(10, 120), cv::FONT_HERSHEY_COMPLEX, 0.5, cv::Scalar(255, 0, 0));
+        cv::imshow("visualLable", visualLabel);
+        cv::waitKey(0);
 //        locate_->predict();
 //        std::cout << trainMat << labelMat << std::endl;
 //        solvePNP_->detect();
