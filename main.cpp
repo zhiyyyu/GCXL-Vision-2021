c21dcbd0fa001fd832881d8621b817d9a8c6a2aa
nArrow4 <3284799532@qq.com>
2021-02-25 21:40:42 +0800

    main file

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ int main() {
 
     cv::Mat qrcode = robot->loadImg(robot->root+robot->qrcode_path);
     cv::Mat barcode = robot->loadImg(robot->root+robot->barcode_path);
-    robot->process(qrcode,  QRCODE_MODE);
+//    cv::Mat material = robot->loadImg();
+//    robot->process(qrcode,  QRCODE_MODE);
     robot->process(barcode, BARCODE_MODE);
 //    robot->process(material, MATERIAL_MODE);
     return 0;
