3b677c20704e52c110e5545ce78d7acf9ee37e41
nArrow4 <3284799532@qq.com>
2021-02-28 17:21:37 +0800

    change api

--- solve/inc/solvePnP.h
+++ solve/inc/solvePnP.h
@@ -15,11 +15,7 @@ namespace QRCode{
     public:
         solvePNP();
         ~solvePNP();
-        void hello();
-        void loadImg(std::string path);
-        void detect();
-        void solve();
-        void display();
+        void solve(std::vector<cv::Point2f> Points2f);
 
         Sophus::SE3 TransformMatrix;
 
@@ -27,18 +23,8 @@ namespace QRCode{
         void setCameraMatrix();
         void setDistCoeffs();
 
-        std::string path = "/home/narrow/QRCode2.bmp";
-        cv::Mat qrcode;
-//        cv::Mat gray;
-//        cv::Mat thresh;
-//        std::vector<std::vector<cv::Point>> contours;
-//        std::vector<cv::RotatedRect> candidates;
         cv::QRCodeDetector detector;
-        std::vector<cv::Point> list;
         cv::Mat result;
-
-        std::vector<cv::Point2d> Points2d;
-        std::vector<cv::Point3d> Points3d;
 
         cv::Mat cameraMatrix;
         cv::Mat distCoeffs;
--- solve/src/solvePnP.cpp
+++ solve/src/solvePnP.cpp
@@ -6,7 +6,8 @@
  */
 #include "solvePnP.h"
 
-#define QRCODE_WIDTH 45.5
+#define MATERIAL_WIDTH 50
+#define MATERIAL_HEIGHT 70
 
 namespace QRCode{
     solvePNP::solvePNP(){
@@ -16,47 +17,6 @@ namespace QRCode{
     solvePNP::~solvePNP(){
 
     }
-//    void solvePNP::hello() {
-//        std::cout << "Hello, World! solvePNP" << std::endl;
-//        return ;
-//    }
-    void solvePNP::loadImg(std::string path){
-        qrcode = cv::imread(path);
-//        cv::imshow("img", qrcode);
-//        cv::waitKey(0);
-        return;
-    }
-
-//    void solvePNP::myQRDetector(cv::Mat qrcode){
-//        cv::cvtColor(qrcode, gray, cv::COLOR_BGR2GRAY);
-//        cv::threshold(gray, thresh, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
-//        cv::findContours(thresh, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-//        for(auto i=0; i<contours.size() ; i++){
-//            candidates.at(i) = cv::minAreaRect(contours[i]);
-//        }
-//        // @TODO add
-//        cv::imshow("thresh", thresh);
-//    }
-
-    void solvePNP::display(){
-        cv::Mat img = qrcode.clone();
-        for(int i=0;i<list.size()-1;i++){
-            cv::line(img, list[i], list[i+1], cv::Scalar(0, 0, 255), 4);
-        }
-        cv::line(img, list[3], list[0], cv::Scalar(0, 0, 255), 4);
-
-        cv::imshow("detected_qrcode", img);
-        cv::imshow("result", result);
-        cv::waitKey(0);
-    }
-
-//    void solvePNP::detect(){
-//        loadImg(path);
-////        myQRDetector(qrcode);
-//        detector.detectAndDecode(qrcode, list, result);
-//
-//        return;
-//    }
 
     void solvePNP::setCameraMatrix(){
         cameraMatrix = cv::Mat(3, 3, CV_64FC1, cv::Scalar::all(0));
@@ -78,14 +38,16 @@ namespace QRCode{
         return;
     }
 
-    void solvePNP::solve(){
+    void solvePNP::solve(std::vector<cv::Point2f> Points2f){
+        std::vector<cv::Point2d> Points2d;
         for(auto i=0; i<4; i++){
-            Points2d.emplace_back(list[i]);
+            Points2d.emplace_back((cv::Point2d)Points2f[i]);
         }
-        Points3d.emplace_back(cv::Point3d(-QRCODE_WIDTH/2, -QRCODE_WIDTH/2, 0));
-        Points3d.emplace_back(cv::Point3d(QRCODE_WIDTH/2, -QRCODE_WIDTH/2, 0));
-        Points3d.emplace_back(cv::Point3d(QRCODE_WIDTH/2, QRCODE_WIDTH/2, 0));
-        Points3d.emplace_back(cv::Point3d(-QRCODE_WIDTH/2, QRCODE_WIDTH/2, 0));
+        std::vector<cv::Point3d> Points3d;
+        Points3d.emplace_back(cv::Point3d(-MATERIAL_WIDTH/2, -MATERIAL_HEIGHT/2, 0));
+        Points3d.emplace_back(cv::Point3d(MATERIAL_WIDTH/2, -MATERIAL_HEIGHT/2, 0));
+        Points3d.emplace_back(cv::Point3d(MATERIAL_WIDTH/2, MATERIAL_HEIGHT/2, 0));
+        Points3d.emplace_back(cv::Point3d(-MATERIAL_WIDTH/2, MATERIAL_HEIGHT/2, 0));
 
         cv::solvePnP(Points3d, Points2d, cameraMatrix, distCoeffs, rvec, tvec, false, cv::SOLVEPNP_ITERATIVE);
 
